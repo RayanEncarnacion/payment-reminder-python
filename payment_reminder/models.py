127fc3402a06cfff0d1943e3d08cbf6d44e4deb4
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-03-03 17:39:56 -0400

    feat: Add models

---
+++ payment_reminder/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.conf import settings
+
+class Audit(models.Model):
+    deleted = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True, name='created_at',)
+    createdBy = models.ForeignKey(settings.AUTH_USER_MODEL, name='created_by', on_delete=models.CASCADE) 
+
+    class Meta:
+        abstract = True
+
+class StateAudit(Audit):
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+class Client(StateAudit):
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        db_table = "client"
+    
+class Project(StateAudit):
+    name = models.CharField(max_length=100, unique=True)
+    amount = models.FloatField()
+    client_id = models.ForeignKey(Client, name='client_id', on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = "project"
+    
+    def get_dates(self):
+        return ProjectDate.get_all_project_dates(self.pk)
+
+class ProjectDate(Audit):
+    project_id = models.ForeignKey(Project, name='project_id', on_delete=models.DO_NOTHING, related_name="dates")
+    day = models.SmallIntegerField()
+
+    class Meta:
+        db_table = "projectDate"
+    
+    @classmethod
+    def get_all_project_dates(self, id):
+        return self.objects.filter(project_id=id)
+    
+class Payment(Audit):
+    project_id = models.ForeignKey(Project, name='project_id', on_delete=models.DO_NOTHING)
+    due_date = models.DateTimeField()
+    amount = models.FloatField()
+    payed = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "payment"
--- server/settings.py
+++ server/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'payment_reminder.apps.PaymentReminderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'app.urls'
+ROOT_URLCONF = 'server.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = 'server.wsgi.application'
 
 
 # Database
