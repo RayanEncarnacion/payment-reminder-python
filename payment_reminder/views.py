18380b40af996d81f94eaca4e9f189dc90e326e5
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-03-04 08:02:05 -0400

    feat: Add templates (login and 404) with Bootstrap 5.3

--- payment_reminder/views.py
+++ payment_reminder/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+
 
 def index(request):
     return HttpResponse("Hello, world! You're at the payment reminder index.")
+
+def login(request):
+    return render(request, "payment_reminder/login.html")
