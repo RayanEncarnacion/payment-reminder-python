dce3bcf79a1fd7235821e44e188f2011b3e7ebd2
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-03-04 07:24:33 -0400

    feat: Add models with fields to django admin panel

--- payment_reminder/admin.py
+++ payment_reminder/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+app_models = apps.get_app_config('payment_reminder').get_models()
+
+for model in app_models:
+    class AdminPanelModel(admin.ModelAdmin):  
+        list_display = [field.name for field in model._meta.fields] 
+                   
+    admin.site.register(model, AdminPanelModel)
