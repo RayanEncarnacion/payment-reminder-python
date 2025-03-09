4c9b783736919a221e4b1150d78d3f9ef20b0f93
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-03-08 22:15:28 -0400

    feat: Handle project creation

--- payment_reminder/views.py
+++ payment_reminder/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .forms import CreateClient, CreateProject
-from .models import Client, User
+from .models import Client, Project
 from django.urls import reverse
 from django.views import generic
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 BASE_TEMPLATES_PATH = "payment_reminder/"
 CLIENT_TEMPLATES_PATH = BASE_TEMPLATES_PATH + "client/"
@@ -29,30 +30,35 @@ def index(request):
 def create_client(request):
     if request.method == "POST":
         form = CreateClient(request.POST)
-        if form.is_valid():
-            client = Client(name=request.POST["name"], email=request.POST["email"], createdBy=User.objects.first())
-            client.save()
         
+        if not form.is_valid():
+            return render(request, CLIENT_TEMPLATES_PATH + "create.html", { "errors": form.errors })
+        
+        client = Client(name=request.POST["name"], email=request.POST["email"], createdBy=request.user)
+        client.save()
         return HttpResponseRedirect(reverse("payment_reminder:clients"))
-
+    
     else:
-        form = CreateClient()
-        
-    return render(request, CLIENT_TEMPLATES_PATH + "create.html", { "form": form })
+        return render(request, CLIENT_TEMPLATES_PATH + "create.html", { "form": CreateClient() })
 
 @login_required
 def create_project(request):
+    
     if request.method == "POST":
-        form = CreateClient(request.POST)
-        if form.is_valid():
-            client = Client(name=request.POST["name"], email=request.POST["email"], createdBy=User.objects.first())
-            client.save()
+        form = CreateProject(request.POST)
         
-        return HttpResponseRedirect(reverse("payment_reminder:clients"))
-
+        if not form.is_valid():
+            return render(request, PROJECT_TEMPLATES_PATH + "create.html", { "form": form.errors })
+        
+        client = get_object_or_404(Client, pk=request.POST["client"])
+        # Add try catch logic
+        project = Project(name=request.POST["name"], 
+                            amount=request.POST["amount"], 
+                            client=client,
+                            createdBy=request.user)
+        project.save()
+        return HttpResponseRedirect(reverse("payment_reminder:create_project"))
     else:
-        form = CreateProject()
-        
-    return render(request, PROJECT_TEMPLATES_PATH + "create.html", { "form": form })
+        return render(request, PROJECT_TEMPLATES_PATH + "create.html", { "form": CreateProject() })
 
 
