3a09e48fb257df306066f393d2e35d6e2e7f0ec1
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-03-09 23:42:13 -0400

    feat: Project's detail, list and create completed behaviour

--- payment_reminder/urls.py
+++ payment_reminder/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.views import generic
+from .models import Client, Project
 
 app_name = "payment_reminder"
 
@@ -9,7 +11,14 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="payment_reminder/login.html", next_page="/client/"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
     path("client/", views.ClientsView.as_view(), name="clients"),
-    path("client/<int:pk>/", views.DetailView.as_view(), name="client_detail"),
     path("client/create/", views.create_client, name="create_client"),
+    path("client/<int:pk>/", 
+         generic.DetailView.as_view(template_name="payment_reminder/client/detail.html", model=Client), 
+         name="client_detail"),
+    
+    path("project/", views.ProjectsView.as_view(), name="projects"),
+    path("project/<int:pk>/", 
+         generic.DetailView.as_view(template_name="payment_reminder/project/detail.html", model=Project), 
+         name="project_detail"),
     path("project/create/", views.create_project, name="create_project"),
 ]
--- payment_reminder/views.py
+++ payment_reminder/views.py
@@ -5,7 +5,6 @@ from .models import Client, Project
 from django.urls import reverse
 from django.views import generic
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
 
 BASE_TEMPLATES_PATH = "payment_reminder/"
 CLIENT_TEMPLATES_PATH = BASE_TEMPLATES_PATH + "client/"
@@ -19,9 +18,16 @@ class ClientsView(generic.ListView):
     def get_queryset(self):
         return Client.objects.filter(deleted__exact=False)
     
-class DetailView(generic.DetailView):
-    model = Client
-    template_name = CLIENT_TEMPLATES_PATH + "detail.html"
+class ProjectsView(generic.ListView):
+    template_name = PROJECT_TEMPLATES_PATH + "index.html"
+
+    def get_queryset(self):
+        return (
+                Project.objects
+                       .select_related("client")
+                       .filter(deleted__exact=False)
+                       .order_by("-createdAt")
+               )
     
 def index(request):
     return HttpResponse("Hello, world! You're at the payment reminder index.")
@@ -46,18 +52,17 @@ def create_project(request):
     
     if request.method == "POST":
         form = CreateProject(request.POST)
-        
         if not form.is_valid():
-            return render(request, PROJECT_TEMPLATES_PATH + "create.html", { "form": form.errors })
+            return render(request, PROJECT_TEMPLATES_PATH + "create.html", { "form": form })
         
-        client = get_object_or_404(Client, pk=request.POST["client"])
-        # Add try catch logic
-        project = Project(name=request.POST["name"], 
-                            amount=request.POST["amount"], 
-                            client=client,
-                            createdBy=request.user)
-        project.save()
-        return HttpResponseRedirect(reverse("payment_reminder:create_project"))
+        Project(
+            name=request.POST["name"], 
+            amount=request.POST["amount"], 
+            client=form.cleaned_data["client"],
+            createdBy=request.user
+        ).save()
+        
+        return HttpResponseRedirect(reverse("payment_reminder:projects"))
     else:
         return render(request, PROJECT_TEMPLATES_PATH + "create.html", { "form": CreateProject() })
 
