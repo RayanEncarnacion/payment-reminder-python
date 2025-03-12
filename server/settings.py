9e8a946807b6b3f15f6a3c28fbd25b910217fb3b
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-03-11 21:51:53 -0400

    fix: Path to correct settings

--- server/settings.py
+++ server/settings.py
@@ -32,13 +32,13 @@ LOGIN_URL = "/login/"
 # Application definition
 
 INSTALLED_APPS = [
-    'payment_reminder.apps.PaymentReminderConfig',
+    'payment_reminder',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'server.urls'
